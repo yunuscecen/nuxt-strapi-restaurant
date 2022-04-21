818e2da42df6598db48218aec5a3042fa050f895
yunuscecen <yunusccn@outlook.com>
2022-04-21 15:05:38 +0300

    added footer

---
+++ client/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <Logo />
+    <span>Copyright &copy; 2022 @</span>
+    <nuxt-link to="/">Privacy Policy</nuxt-link>
+    <nuxt-link to="/">Terms of Use</nuxt-link>
+    <nuxt-link to="/">Contact</nuxt-link>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.logo {
+}
+</style>
